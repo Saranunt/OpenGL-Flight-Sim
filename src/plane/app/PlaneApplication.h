d8a0afa158ef3037be83713e42368c8a76d7af67
Pongpon Butsrimart <wavenakab2019@gmail.com>
2025-12-16 17:33:03 +0700

    ✨ Implement split-screen functionality and player input handling

--- src/plane/app/PlaneApplication.h
+++ src/plane/app/PlaneApplication.h
@@ -9,6 +9,7 @@
 #include <glm/glm.hpp>
 
 #include <memory>
+#include <array>
 
 #include "core/AppConfig.h"
 #include "core/CameraController.h"
@@ -45,9 +46,10 @@ namespace plane::app
         bool InitializeWindow();
         bool InitializeGlad();
         void InitializeScene();
+        void InitializePlayers();
         void Render();
         void RenderDepthPass(const glm::mat4& lightSpaceMatrix);
-        void RenderColorPass(const glm::mat4& projection, const glm::mat4& view, const glm::mat4& lightSpaceMatrix);
+        void RenderColorPass(const glm::mat4& projection, const glm::mat4& view, const glm::mat4& lightSpaceMatrix, const core::CameraRig& cameraRig);
         void RenderSceneGeometry(Shader& shader, bool bindTextures);
         glm::mat4 CalculateLightSpaceMatrix() const;
 
@@ -56,6 +58,15 @@ namespace plane::app
         static void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
 
         static PlaneApplication* s_instance_;
+
+        struct PlayerContext
+        {
+            core::PlaneState state;
+            core::CameraRig cameraRig;
+            core::CameraController cameraController;
+            float fireCooldown { 0.0f };
+            float fireRatePerSec { 8.0f }; // bullets per second
+        };
 
         GLFWwindow* window_ { nullptr };
         std::unique_ptr<Shader> shader_;
@@ -69,10 +80,9 @@ namespace plane::app
         render::ShadowMap shadowMap_;
         world::IslandManager islandManager_;
 
-        core::PlaneState planeState_;
-        core::CameraRig cameraRig_;
+        std::array<PlayerContext, 2> players_;
+        std::array<input::InputBindings, 2> inputBindings_;
         core::TimingState timingState_;
-        core::CameraController cameraController_;
         entities::PlaneController planeController_;
         input::InputHandler inputHandler_;
         physics::CollisionSystem collisionSystem_;
@@ -83,7 +93,5 @@ namespace plane::app
         features::multiplayer::MultiplayerManager multiplayerManager_;
 
         glm::vec3 lightDirection_ { -0.3f, -1.0f, -0.3f };
-        bool fireHeldLastFrame_ { false };
     };
 }
-
